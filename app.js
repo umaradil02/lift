c93aa694fe52ee55b90fadbf692a8cd2c5d65db3
umaradil02 <158709535+umaradil02@users.noreply.github.com>
2024-08-03 06:41:42 +0500

    chain logic

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 const lift = document.querySelector("#lift");
+const chain = document.querySelector("#chain");
+let chaintime = 1200
+console.log(chain)
 let floor = document.querySelectorAll(".floor");
 const text = document.querySelector(".text");
 const liftDoors = document.querySelector('.lift-doors');
 const classes = ["ground", "first", "second", "third", "four"]
 let moving = false
+const liftHeights = {
+    ground: 900,
+    first: 750,
+    second: 650,
+    third: 550,
+    four: 450
+};
+
+const chainHeights = {
+    ground: 600,
+    first: 500,
+    second: 335,
+    third: 235,
+    four: 150
+};
 floor.forEach((key, index) => {
     key.addEventListener("click", () => {
         if(!moving){
-            moving = true
+            moving = true;
+            let selectfloor = classes[index];
+            console.log(selectfloor)
+            chain.style.transition = `height ${chaintime}ms`
+            chain.style.height = `${chainHeights[selectfloor]}px`
         lift.src = "liftclose.jpg";
         lift.className = '';
-        lift.classList.add(classes[index])
+        lift.classList.add(selectfloor)
             key.style.color = "red";
             key.style.border = "2px solid red";
             text.textContent = index
@@ -20,8 +42,8 @@ floor.forEach((key, index) => {
             lift.src = "liftopen.jpg"
             key.style.color = "white";
             key.style.border = "";
-        }, 3000);
-        moving = false
+            moving = false;
+        }, chaintime);
     }
     })
 })
